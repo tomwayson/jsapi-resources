0ba7c2ae7d4772de9935864ed494412908efd886
Tom Wayson <tom@tomwayson.com>
2018-04-28 18:46:11 -0700

    consolodate esri imports and only load map/layer after componet mounts

--- 4.x/webpack/demo/src/components/webmapview.tsx
+++ 4.x/webpack/demo/src/components/webmapview.tsx
@@ -1,21 +1,36 @@
+import FeatureLayer = require("esri/layers/FeatureLayer");
 import WebMap = require("esri/WebMap");
 import MapView = require("esri/views/SceneView");
 import * as React from "react";
 
-interface ComponentProps {
-  webmap: WebMap;
-  onload: (view: MapView) => void;
-}
-
-export class WebMapComponent extends React.Component<ComponentProps, {}> {
+export class WebMapComponent extends React.Component<{}> {
   mapDiv: any;
 
   componentDidMount() {
+    const featureLayer = new FeatureLayer({
+      id: "states",
+      portalItem: {
+        id: "b234a118ab6b4c91908a1cf677941702"
+      },
+      outFields: ["NAME", "STATE_NAME", "VACANT", "HSE_UNITS"],
+      title: "U.S. counties"
+    });
+    
+    const webmap = new WebMap({
+      portalItem: {
+        id: "3ff64504498c4e9581a7a754412b6a9e"
+      },
+      layers: [featureLayer]
+    });
+    
     const view = new MapView({
-      map: this.props.webmap,
+      map: webmap,
       container: this.mapDiv
     });
-    this.props.onload(view);
+
+    featureLayer.when(() => {
+      view.goTo({ target: featureLayer.fullExtent });
+    });
   }
 
   render() {
